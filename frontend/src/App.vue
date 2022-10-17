97904dfee9977a8660314cd8ce60f68aa044a6ec
Henrique Pimentel <henrique.pimentel@srconsulting.io>
2022-10-17 08:55:24 -0300

    Ajustes iniciais, header e footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="container-fluid p-0">
+    <HeaderPrincipal />
+    <FooterPrincipal />
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import HeaderPrincipal from './components/HeaderPrincipal.vue';
+import FooterPrincipal from './components/FooterPrincipal.vue';
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: { HeaderPrincipal, FooterPrincipal }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ frontend/src/components/FooterPrincipal.vue
@@ -0,0 +1,33 @@
+<template>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col d-flex">
+                    <img src="" alt="">
+                    <p>Biblioteca Pública da Prefeitura Municipal de Brás Cubas</p>
+                </div>
+                <div class="col"><p>Powered by LH Technology Company</p></div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: 'FooterPrincipal'
+})
+</script>
+
+<style scoped>
+footer {
+    background: #7b0000;
+    width: 100%;
+    height: 15vh;
+}
+img {
+    width: 20%;
+    height: 100%;
+}
+</style>
