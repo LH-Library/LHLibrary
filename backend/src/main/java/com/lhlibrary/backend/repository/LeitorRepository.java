16b4dfb98f9378925235ac639f4d7c5eef8db5df
lorenzasilveira <lorenzaasilveira@gmail.com>
2022-11-14 21:29:46 -0300

    LHL-9 adicionado crud de leitor

--- backend/src/main/java/com/lhlibrary/backend/repository/LeitorRepository.java
+++ backend/src/main/java/com/lhlibrary/backend/repository/LeitorRepository.java
@@ -2,8 +2,14 @@ package com.lhlibrary.backend.repository;
 
 import com.lhlibrary.backend.entity.Leitor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LeitorRepository extends JpaRepository<Leitor, Long> {
+    @Query("SELECT l FROM Leitor l WHERE lower(l.nome) like lower(concat('%', :filtro,'%')) or lower(l.cpf) like lower(concat('%', :filtro,'%'))")
+    List<Leitor> findByNomeOrCpfContainingIgnoreCase(@Param("filtro") String filtro);
 }
--- backend/src/main/java/com/lhlibrary/backend/service/LeitorService.java
+++ backend/src/main/java/com/lhlibrary/backend/service/LeitorService.java
@@ -1,8 +1,19 @@
 package com.lhlibrary.backend.service;
 
 import com.lhlibrary.backend.entity.Leitor;
+import com.lhlibrary.backend.entity.Livro;
 import org.springframework.lang.NonNull;
+
+import java.util.List;
 
 public interface LeitorService {
     Leitor save(@NonNull Leitor leitor);
+
+    Leitor findById(@NonNull Long id);
+
+    List<Leitor> findAll();
+
+    Leitor updateLeitor(@NonNull Leitor leitor);
+
+    List<Leitor> search(String filtro);
 }
